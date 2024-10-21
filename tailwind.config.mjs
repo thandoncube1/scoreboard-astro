b829d63637feb5b6ddc110dc1f5c6090fdba6ff2
thandoncube1 <ncubethando20@gmail.com>
2024-10-21 16:56:14 -0400

    Working on the UI for the scoreboard.

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+    content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+    theme: {
+        screens: {
+            sm: "576px",
+            // => @media (min-width: 576px) { ... }
+
+            md: "960px",
+            // => @media (min-width: 960px) { ... }
+
+            lg: "1440px",
+            // => @media (min-width: 1440px) { ... }
+        },
+        extend: {},
+    },
+    plugins: [],
+};
