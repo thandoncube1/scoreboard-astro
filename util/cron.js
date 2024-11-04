bb237a5747b4d4ad984890ef8c7e10f8d0b3fe2b
thandoncube1 <ncubethando20@gmail.com>
2024-11-04 05:11:05 -0500

    Implementing updates to the cron-job

--- util/cron.js
+++ util/cron.js
@@ -1,19 +1,28 @@
 import cron from "npm:node-cron";
 import { saveFileData, formattedDate, FileManager } from "./helper.js";
+// Import the webCrawlers
+import { scrapeNBAGameDetails, scrapeNBAGameStats } from "../server.js";
 
-const fileManager = new FileManager(formattedDate);
+const fileManager = new FileManager(formattedDate, 'cronjob_log', 'txt');
+// Get the time zone
+const timezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
 
 // Write a log everytime the cron runs
-function createLogs() {
-    console.log('Cron job executed at: ', new Date().toLocaleString(), " at America/New_York");
+function createLogs(details) {
+    console.log('Cron job executed at: ', new Date().toLocaleString(), " at " + timezone);
 
-    fileManager.
+    fileManager.appendLogs(`Cron job executed at: , ${new Date().toLocaleString()}, at ${timezone} - ${details}`);
 }
 
 // Schedule a cron job to run every minute
-cron.schedule("0 5 * * *", () => {
-    createLogs();
+cron.schedule("0 5 * * *", async () => {
+    try {
+        const result = await scrapeNBAGameDetails(formattedDate);
+        createLogs(result);
+    } catch (error) {
+        createLogs(error);
+    };
 }, {
     scheduled: true,
-    timezone: "America/New_York"
+    timezone
 });
